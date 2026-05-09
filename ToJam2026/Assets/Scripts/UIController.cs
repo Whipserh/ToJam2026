e78475901b2c8f00d5b04ebb4cfb5ccb120c4067
Mac Mealey <148462018+Maccy-0@users.noreply.github.com>
2026-05-09 19:25:44 -0400

    Final enemy

--- ToJam2026/Assets/Scripts/UIController.cs
+++ ToJam2026/Assets/Scripts/UIController.cs
@@ -24,7 +24,7 @@ public class UIController : MonoBehaviour
     void Start()
     {
         coins = 0;
-        maxCoins = 152;//better in the future
+        maxCoins = 191;//better in the future
         coinCounter.text = "Coins: " + coins + "/" + maxCoins;
     }
 
